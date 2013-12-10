63001c146a12a689f791a58b363d16c0bd59c9ec
wso277 <wily1993@gmail.com>
2013-12-10 16:49:56 +0000

    Print done

--- kenken.pl
+++ kenken.pl
@@ -5,10 +5,10 @@ testBoard([[cell(1, _), cell(2, _), cell(2, _), cell(3, _)],
 		   [cell(6, _), cell(4, _), cell(5, _), cell(5, _)],
 		   [cell(6, _), cell(7, _), cell(7, _), cell(8, _)]]).
 
-testBoardPrint([[cell(1, 5), cell(2, 5), cell(2, 5), cell(3, 5)],
-		   [cell(1, 5), cell(4, 5), cell(5, 5), cell(3, 5)],
-		   [cell(6, 5), cell(4, 5), cell(5, 5), cell(5, 5)],
-		   [cell(6, 5), cell(7, 5), cell(7, 5), cell(8, 5)]]).
+testBoardPrint([[cell(1, 5), cell(2, 5), cell(2, 555), cell(3, 5)],
+		   [cell(1, 55), cell(4, 5), cell(5, 5), cell(3, 5)],
+		   [cell(6, 5), cell(4, 55), cell(5, 5), cell(5, 5)],
+		   [cell(6, 5), cell(7, 5), cell(7, 5), cell(8, 55)]]).
 
 testFields([field(1, '-', 1),
 			field(2, '/', 2),
@@ -102,37 +102,72 @@ printBoard :- testBoardPrint(Board),
 			  printBoard(Board), 
 			  printBottomBorder(Board).
 
-printTopBorder(Board) :- length(Board, Size), 
-						 printHorizBorder(Size).
+printTopBorder(Board) :- length(Board, Size), asserta(size(Size)),
+						 write('   '), write('╔'),
+						 printHorizTopBorder(Size).
 
-printBoard([]).
-printBoard([B | Bs]) :- printRow(B),
-						printBoard(Bs).
+printBoard(Board) :- length(Board, Size), printBoard(Board, Size, 1).
 
-printRow([C1]) :- cell(_, Value1) = C1, 
-					 write(Value1), 
-					 write('║\n').
+printBoard([], _, _).
+printBoard([B1, B2 | Bs], Size, RowIt) :- printNumber(RowIt), write('║'), 
+									 printRow(B1),
+									 write('   ║'),
+									 printHorizMidBorder(B1, B2),
+									 RowIt1 is RowIt + 1,
+									 printBoard([B2 | Bs], Size, RowIt1).
+
+printBoard([B1 | BS], Size, RowIt) :- printNumber(RowIt), write('║'), 
+									 printRow(B1),
+									 RowIt1 is RowIt + 1,
+									 printBoard(Bs, Size, RowIt1).
+
+printRow([C1]) :- cell(_, Value1) = C1,  
+				  printNumber(Value1),
+				  write('║\n').
 
 printRow([C1, C2 | Cs]) :- cell(FieldID1, Value1) = C1, 
 						   cell(FieldID2, Value2) = C2, 
 						   FieldID1 = FieldID2, 
-						   write(Value1), 
-						   write(' |'), 
+						   printNumber(Value1),
+						   write('|'), 
 						   printRow([C2 | Cs]).
 
 printRow([C1 | Cs]) :- cell(_, Value1) = C1,
-					   write(Value1), 
+					   printNumber(Value1),
 					   write('║'), 
 					   printRow(Cs).
 
-printBottomBorder(Board) :- length(Board, Size), printHorizBorder(Size), printBottomNumbers(Size).
+printBottomBorder(Board) :- length(Board, Size), write('   ╚'), printHorizBotBorder(Size), write(' '), printBottomNumbers(Size).
 
-printHorizBorder(0):-  write('\n').
+printHorizTopBorder(1):-  write('═══╗'), write('\n').
 
-printHorizBorder(Size) :- write('═══'), Size1 is Size - 1, printHorizBorder(Size1).
+printHorizTopBorder(Size) :- write('════'), Size1 is Size - 1, printHorizTopBorder(Size1).
 
-printBottomNumbers(Size) :- printBottomNumbers(Size, 1).
+printHorizBotBorder(1):-  write('═══╝'), write('\n').
 
-printBottomNumbers(Size, Size) :- write(Size).
+printHorizBotBorder(Size) :- write('════'), Size1 is Size - 1, printHorizBotBorder(Size1).
 
-printBottomNumbers(Size, Number) :- write(' '), write(Number), write(' '), Number1 is Number + 1, printBottomNumbers(Size, Number1).
+printBottomNumbers(Size) :- write('   '), printBottomNumbers(Size, 1).
+
+printBottomNumbers(Size, Size) :- printNumber(Size).
+
+printBottomNumbers(Size, Number) :- printNumber(Number), write(' '), Number1 is Number + 1, printBottomNumbers(Size, Number1).
+
+printHorizMidBorder([R1], [R2]) :- cell(FieldID1, _) = R1, 
+								   cell(FieldID2, _) = R2, 
+								   FieldID1 = FieldID2, 
+								   write('───║\n'). 
+
+printHorizMidBorder([R1], [R2]) :- write('═══║\n').
+
+printHorizMidBorder([R1 | R1s], [R2 | R2s]) :- cell(FieldID1, _) = R1, 
+											   cell(FieldID2, _) = R2, 
+											   FieldID1 = FieldID2, 
+											   write('───═'), 
+											   printHorizMidBorder(R1s, R2s). 
+
+printHorizMidBorder([R1 | R1s], [R2 | R2s]) :- write('════'), printHorizMidBorder(R1s, R2s).
+
+printNumber(Number) :- Number > 99, write(Number).
+printNumber(Number) :- Number > 9, write(' '), write(Number).
+printNumber(Number) :- write(' '), write(Number), write(' ').
